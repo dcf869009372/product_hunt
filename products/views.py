a5e8cc283ba7988f98ae1e57f94f37e07f7db347
qiufeng <869009372@qq.com>
2018-09-19 18:04:16 +0800

    完成了发布/提交产品到服务器（后台）

--- products/views.py
+++ products/views.py
@@ -1,18 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required  # 登录授权，只有登录了才能进行相应的操作，比如发布产品
+from .models import Product
+from django.utils import timezone
 # Create your views here.
 
 
 def product_list(request):
     return render(request,'product_list.html')
 
+@login_required
 def publish(request):
     if request.method == 'GET':
         return render(request,'publish.html')
     elif request.method =='POST':
-        app_name = request.POST['APP名称']
-        intro= request.POST['介绍']
+        title = request.POST['APP名称']
+        intro = request.POST['介绍']
         url = request.POST['APP链接']
-        icon = request.POST['APP图标']
-        image = request.POST['大图']
-        return render(request,'publish.html')
+        try:
+            icon = request.FILES['APP图标']
+            image = request.FILES['大图']
+
+            product = Product()
+            product.title = title
+            product.intro = intro
+            product.url = url
+            product.icon = icon
+            product.image = image
+
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+
+            product.save()
+            return redirect('主页')
+        except Exception as err:
+            return render(request,'publish.html',{'错误':'请上传图片'})
+
