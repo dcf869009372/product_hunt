886ec4f7fdeec7812d6f63f471f8d2b57016b98e
qiufeng <869009372@qq.com>
2018-09-19 16:19:02 +0800

    初步完成了APP发布页面

--- products/urls.py
+++ products/urls.py
@@ -17,8 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup,name='注册页面'),
-    path('login/', views.login,name='登录页面'),
-    path('logout/', views.logout,name='退出页面'),
+    path('publish/', views.publish,name='发布页面'),
 
 ]
